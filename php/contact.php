ffe8592dc9addee13257a4338b6b5834fab2f96c
Isaac Suaste <isuastem@gmail.com>
2016-09-24 13:38:37 -0500

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,95 @@
 <?php
-// Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
-  // Detect & prevent header injections
-  $test = "/(content-type|bcc:|cc:|to:)/i";
-  foreach ( $_POST as $key => $val ) {
-    if ( preg_match( $test, $val ) ) {
-      exit;
-    }
-  }
+// CHANGE EMAIL ADDRESS ON LINE 54.
 
-  // Replace "yourmail@domain.com" with your email
-  mail( "hola@isaacsuaste.com", $_POST['subject'], $_POST['message'], "From:" . $_POST['email'] );
+
+if(!$_POST) exit;
+
+// Email address verification, do not edit.
+function isEmail($email) {
+	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+}
+
+if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+
+$name       = $_POST['name'];
+$email    = $_POST['email'];
+$subject    = $_POST['subject'];
+$message   = $_POST['message'];
+
+
+$verify     = $_POST['verify'];
+
+if(trim($name) == '') {
+	echo '<div class="error_message">Por favor comparte tu nombre.</div>';
+	exit();
+} else if(trim($email) == '') {
+	echo '<div class="error_message">Por favor introduce una dirección valida.</div>';
+	exit();
+} else if(!isEmail($email)) {
+	echo '<div class="error_message">Has introducido una dirección de correo electrónico no válida, vuelva a intentarlo.
+</div>';
+	exit();
+}
+
+if(trim($message) == '') {
+	echo '<div class="error_message">Por favor comparteme tus dudas.</div>';
+	exit();
+}
+
+if(get_magic_quotes_gpc()) {
+	$message = stripslashes($message);
+}
+
+
+// Configuration option.
+// Enter the email address that you want to emails to be sent to.
+// Example $address = "joe.doe@yourdomain.com";
+
+//$address = "example@example.com";
+$address = "hola@isaacsuaste.com";
+
+
+// Configuration option.
+// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+
+// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+
+$e_subject = 'Te ha llegado un correo de: ' . $name . '.';
+
+
+// Configuration option.
+// You can change this if you feel that you need to.
+// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+
+$e_body = "Ha compartido su propuesta $name con respecto a $subject, su propuesta es la siguiente." . PHP_EOL . PHP_EOL;
+$e_content = "\"$nombre\"" . PHP_EOL . PHP_EOL;
+$e_content = "\"$email\"" . PHP_EOL . PHP_EOL;
+$e_content = "\"$subject\"" . PHP_EOL . PHP_EOL;
+
+$e_content = "\"$message\"" . PHP_EOL . PHP_EOL;
+$e_reply = "Puede contactar con $name vía email, $email .";
+
+$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+
+$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+if(mail($address, $e_subject, $msg, $headers)) {
+
+	// Email has sent successfully, echo a success page.
+
+	echo "<fieldset>";
+	echo "<div id='success_page'>";
+	echo "<h4 class='highlight'>Muchas gracias <strong>$name</strong>, en breve me comunicó contigo.</h4>";
+	echo "</div>";
+	echo "</fieldset>";
+
+} else {
+
+	echo 'ERROR!';
 
 }
-?>
